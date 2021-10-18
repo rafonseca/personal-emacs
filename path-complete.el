c947fbdbad178a8e36aaa950ce3af2a1c2c6d28e
Renan Fonseca <renanfonseca@gmail.com>
2021-10-19 00:49:56 +0200

    Add path-complete

---
+++ path-complete.el
@@ -0,0 +1,49 @@
+;;; path-complete.el --- Basic path completion       -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Renan Fonseca
+
+;; Author: Renan Fonseca <renanfonseca@gmail.com>
+;; Keywords: convenience, files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(defun get-file-completions (path)
+ (let* ((file (file-name-nondirectory path))
+        (dir (or (file-name-directory path) "")))
+   (mapcar (lambda (i) (concat dir i )) (file-name-all-completions file dir))))
+
+
+(defun complete-file-name ()
+  (ignore-errors
+    (let* ((bounds (bounds-of-thing-at-point 'filename))
+           (start (car bounds))
+           (end (cdr bounds))
+           (thing (buffer-substring-no-properties start end))
+           (collection (get-file-completions thing)))
+      (when collection
+        (list start end collection :exclusive 'no)))))
+
+
+(add-hook 'completion-at-point-functions 'complete-file-name 0 nil)
+
+
+(provide 'path-complete)
+;;; path-complete.el ends here
